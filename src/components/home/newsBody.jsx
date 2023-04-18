1f7d6a234ed8de99220494a638481418caa6243f
Sebastián Remaggi <seba.rf96@gmail.com>
2023-04-17 23:01:25 -0400

    cambios grid news

--- src/components/home/newsBody.jsx
+++ src/components/home/newsBody.jsx
@@ -6,19 +6,14 @@ import { WelcomeCard } from "../welcomeCard";
 
 export const NewsBody = () => {
   return (
-    <div className="h-screen flex items-center">
-      <div className="grid grid-cols-2 gap-2 w-full ">
-        <div className="col-span-2 lg:col-span-1">
-          <CarouselFade title="Casa principal" />
-        </div>
-        <div className="col-span-2 lg:col-span-1">
-          <WelcomeCard title="Bienvenid@s" />
-        </div>
-        <div className="col-span-2 lg:col-span-1">
+    <div className="h-screen w-screen flex mt-12 ">
+      <div className="grid grid-cols-4 w-full h-full items-center gap-y-0 auto-rows-max">
+        <div className=" bg-red-600 col-span-2">
           <CarouselFade title="Casa inferior" />
         </div>
-        <div className="col-span-2 lg:col-span-1">
-          <WelcomeCard title="Iniciar sesión" />
+
+        <div className=" bg-red-600 col-span-2">
+          <CarouselFade title="Casa inferior" />
         </div>
       </div>
     </div>
--- src/components/welcomeCard.jsx
+++ src/components/welcomeCard.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export const WelcomeCard = (props) => {
   const { title } = props;
   return (
-    <div className="h-full max-w-[700px] mx-3 rounded-lg shadow-lg bg-[#F3F3F3] text-gray-700 hidden md:block p-3">
+    <div className="h-full max-w-[700px] mx-3 rounded-lg shadow-lg bg-[#F3F3F3] text-gray-700 p-3">
       <div className="grid grid-cols-4">
         <div className="col-span-4   flex justify-center text-gray-600">
           <h1 className="text-2xl">{title}</h1>
